022cef8687d189468c7924d4eaec7a23d65f7e18
Ipos123 <100823788+Ipos123@users.noreply.github.com>
2022-08-18 13:34:02 +0530

    Update settings.py

--- Ipos/settings.py
+++ Ipos/settings.py
@@ -131,5 +131,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 
